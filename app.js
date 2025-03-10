94e56d7f0d6a1816f2e7561fbceed11b9b0e5561
hsinny <hsinnyliu@gmail.com>
2025-03-10 20:14:27 +0800

    wip: 實驗 OpenAI 聊天功能和串流回覆模式

---
+++ app.js
@@ -0,0 +1,57 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+async function chat() {
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "system",
+          "content": "你是對咖啡知識很了解的人",
+        },
+        {
+          "role": "user",
+          "content": "請推薦我一款好喝的單品咖啡，10個字內的簡答"
+        },
+      ],
+    })
+    console.log(completion.choices[0].message.content);
+  } catch (error) {
+    console.error(error)
+  }
+}
+// chat();
+
+// 串流回覆模式
+async function streamChat() {
+  try {
+    const stream = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "system",
+          "content": "你是對咖啡知識很了解的人",
+        },
+        {
+          "role": "user",
+          "content": "請推薦我一款好喝的單品咖啡，10個字內的簡答",
+        }
+      ],
+      stream: true, // 啟用串流回覆
+    });
+
+    for await (const chunk of stream) {
+      console.log(chunk.choices[0]?.delta?.content || '');
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+streamChat();
