a4b9aebb4aa5a62d806df3b7d52597fea225c99e
hsinny <hsinnyliu@gmail.com>
2025-03-12 09:57:08 +0800

    feat: 前端串接 chat API 和新增聊天介面操作顯示訊息
    - 後端支援跨域請求

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from 'express';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import chatRoutes from './routes/chatRoutes.js';
@@ -11,6 +12,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
